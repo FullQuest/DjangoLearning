7a6886a387f35181b4e2d35cff53850e010c7e09
Elisey <fullquestmusic@gmail.com>
2022-01-12 11:18:53 +0300

    Added edit and delete methods into TUTORIAL project

--- devsearch/projects/views.py
+++ devsearch/projects/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import Project
 from .forms import ProjectForm
+
 
 def projects(request):
     projects = Project.objects.all()  # pylint: disable=maybe-no-member
@@ -16,7 +17,42 @@ def project(request, pk):
 
 def createProject(request):
     form = ProjectForm()
+
+    if request.method == 'POST':
+        form = ProjectForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('projects')
+        # print(request.POST)
+
     context = {'form': form}
     return render(request, "projects/project_form.html", context)
 
 
+def updateProject(request, pk):
+    project = Project.objects.get(id=pk)
+    form = ProjectForm(instance=project)
+
+    if request.method == 'POST':
+        form = ProjectForm(request.POST, instance=project)
+
+        if form.is_valid():
+            form.save()
+            return redirect('projects')
+        # print(request.POST)
+
+    context = {'form': form}
+    return render(request, "projects/project_form.html", context)
+
+
+def deleteProject(request, pk):
+    project = Project.objects.get(id=pk) 
+
+    if request.method == 'POST':
+        project.delete()
+        return redirect('projects')
+
+    context = {'object': project}
+    return render(request, 'projects/delete_template.html', context)
+
+    
