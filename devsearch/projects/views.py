303926666706f1293c5ee0044a4dda866b75f465
Elisey <fullquestmusic@gmail.com>
2022-01-12 11:20:22 +0300

    little tweak into views

--- devsearch/projects/views.py
+++ devsearch/projects/views.py
@@ -45,7 +45,7 @@ def updateProject(request, pk):
     return render(request, "projects/project_form.html", context)
 
 
-def deleteProject(request, pk):
+def deleteProject(request, pk): 
     project = Project.objects.get(id=pk) 
 
     if request.method == 'POST':
